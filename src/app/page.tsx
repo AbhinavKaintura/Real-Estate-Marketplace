47cda5ecb5b40a444abc07c7adab56f94067c9bb
AbhinavKaintura <abhinavkaintura001@gmail.com>
2025-03-21 15:53:56 +0530

    Nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import Navbar from '@/components/nav-bar/page';
+import { m as motion, LazyMotion, domAnimation } from 'framer-motion';
+import Head from 'next/head';
+
 
 export default function Home() {
   return (
 
-    <div>Hlo from the home page
-
+    <LazyMotion features={domAnimation} >
+    <div className="min-h-screen">
+        <Head>
+          <title>Endurance States | Design Creates Culture</title>
+          <meta name="description" content="We are a design-driven agency creating meaningful digital experiences." />
+        </Head>
+      <main>
+        <Navbar/>
+      </main>
       <Link href="/price-prediction">Predict Price</Link>
+
     </div>
+    </LazyMotion>
+    
   )
 }
