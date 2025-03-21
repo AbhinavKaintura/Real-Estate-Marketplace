d059352f34396f5b270927acbddb67a294a9a81e
AbhinavKaintura <abhinavkaintura001@gmail.com>
2025-03-21 12:30:29 +0530

    Initial commits

---
+++ src/app/price-prediction/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PricePredictionPage() {
+  return (
+    <div>
+      <h1>Price Prediction</h1>
+      <p>Coming soon...</p>
+    </div>
+  );
+}
